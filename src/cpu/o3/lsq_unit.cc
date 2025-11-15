47f9e50ca221f913e8040aa9f4fc08d43fa76785
Dhruv <khatridhruv35@gmail.com>
2025-11-15 15:40:06 -0500

    call function for getting wib index

--- src/cpu/o3/lsq_unit.cc
+++ src/cpu/o3/lsq_unit.cc
@@ -1601,21 +1601,23 @@ LSQUnit::read(LSQRequest *request, ssize_t load_idx)
 
         // get the bit vector index for the miss
         int wib_index = -1;
-        // wib_index = wib->getIndex();
+        ThreadID tid = load_inst->threadNumber;
+        if (iewStage->rob->getLoadVectorPtr(tid, wib_index)) {
 
-        // Setting wait bit for every blocked load
-        // Should also check if the WIB can issue a new b-vector index?
-        for (int i = 0; i < load_inst->numDestRegs(); i++) {
-            // Mark register as waiting ( do we need to check if not pinned?)
-            // if (inst->renamedDestIdx(i)->
-            //         getNumPinnedWritesToComplete() == 0) {
-                DPRINTF(LSQUnit,"Setting Destination Register wait bit %i (%s)"
-                        "with WIB Index (%d)\n",
-                        load_inst->renamedDestIdx(i)->index(),
-                        load_inst->renamedDestIdx(i)->className(),
-                        wib_index);
-                PhysRegIdPtr dest_reg = load_inst->renamedDestIdx(i);
-                iewStage->instQueue.setWait(dest_reg->flatIndex(), wib_index);
+            // Setting wait bit for every blocked load
+            // Should also check if the WIB can issue a new b-vector index?
+            for (int i = 0; i < load_inst->numDestRegs(); i++) {
+                // Mark register as waiting ( do we need to check if not pinned?)
+                // if (inst->renamedDestIdx(i)->
+                //         getNumPinnedWritesToComplete() == 0) {
+                    DPRINTF(LSQUnit,"Setting Destination Register wait bit %i (%s)"
+                            "with WIB Index (%d)\n",
+                            load_inst->renamedDestIdx(i)->index(),
+                            load_inst->renamedDestIdx(i)->className(),
+                            wib_index);
+                    PhysRegIdPtr dest_reg = load_inst->renamedDestIdx(i);
+                    iewStage->instQueue.setWait(dest_reg->flatIndex(), wib_index);
+                    }
         }
     }
 
